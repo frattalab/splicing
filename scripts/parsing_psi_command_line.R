9927dfd9f6ab437f9a346931485c6ac4b618ef15
aleighbrown <brown.annaleigh@gmail.com>
2020-04-07 17:02:04 +0100

    update parser

--- scripts/parsing_psi_command_line.R
+++ scripts/parsing_psi_command_line.R
@@ -21,14 +21,29 @@ parse_voila_tsv = function(file_path){
         voila_org =  as.data.table(clean_names(fread(file_path)))
         colsToSplit = c("e_psi_per_lsv_junction",
                         "var_e_psi_per_lsv_junction", 
-                        "junctions_coords")
+                        "junctions_coords",
+                        "exons_coords",
+                        "de_novo_junctions")
         voila_melt = cSplit(voila_org, colsToSplit,';', "long")
         #the splitting and melting can produce non unique rows, so remove those
         voila_melt = unique(voila_melt)
         #also can result in some rows that have NA in their psi columns
         voila_melt = voila_melt[!is.na(e_psi_per_lsv_junction)]
+        # split the exon_coords to have an exon start and exon end
+        voila_melt[,c("exon_start","exon_end") := tstrsplit(exons_coords, "-")]
+        # same with the junctions and the retained introns
         #now to make a unique identifier cmbined the lsv_id and junction coordinates
         voila_melt[,lsv_junc := paste(lsv_id, junctions_coords,sep = "_")]
+        
+        voila_melt[,c("junc_start","junc_end") := tstrsplit(junctions_coords, "-")]
+        voila_melt[,c("ir_start","ir_end") := tstrsplit(ir_coords, "-")]
+        # I find this column irritating
+        voila_melt$ucsc_lsv_link = NULL
+        # remove these as they're now redundant
+        voila_melt$junctions_coords = NULL
+        voila_melt$ir_coords = NULL
+        voila_melt$exons_coords = NULL
+        
         # helpful column
         voila_melt[,paste_into_igv_junction := paste0(chr, ":",junc_start, "-",junc_end)]
         # add on the target or source
@@ -43,6 +58,7 @@ parse_voila_tsv = function(file_path){
         return("File not found")
     }
 }
+
 
 
 parsed = parse_voila_tsv(opt$input)
