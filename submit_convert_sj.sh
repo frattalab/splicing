b63eee878dfa6a1f2e89531b49d9e6f7b5ffd106
aleighbrown <brown.annaleigh@gmail.com>
2021-01-13 20:01:25 +0000

    memm

--- submit_convert_sj.sh
+++ submit_convert_sj.sh
@@ -24,7 +24,7 @@ snakemake -s rules/convert_sj_to_psi.smk \
 --jobscript cluster_qsub.sh \
 --cluster-config config/cluster.yaml \
 --cluster-sync "qsub -l h_vmem={cluster.h_vmem},h_rt={cluster.h_rt} -o $FOLDER {cluster.submission_string}" \
--j 100 \
+-j 50 \
 --nolock \
 --rerun-incomplete \
 --latency-wait 100
