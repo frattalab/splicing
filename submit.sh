2987f603fd89c2a0bb5792ad452a75a07260e217
aleighbrown <brown.annaleigh@gmail.com>
2021-03-02 10:45:02 +0000

    getting the env to work

--- submit.sh
+++ submit.sh
@@ -26,11 +26,11 @@ cp config/config.yaml ${FOLDER}/${RUN_NAME}_config.yaml
 cp config/comparisons.yaml ${FOLDER}/${RUN_NAME}_comparisons.yaml
 # if [[ ${1} == "transcriptome_assembly"  || ${1} == "exon_beds"]]
 
-# if [${1} == "build"]; then
-#   CONDATAG=" "
-# else
-#   CONDATAG="--use-conda"
-# fi
+if [${1} == "build"]; then
+  CONDATAG=" "
+else
+  CONDATAG="--use-conda"
+fi
 CONDATAG=" "
 snakemake -s ${WORKFLOW} \
 --jobscript cluster_qsub.sh \
