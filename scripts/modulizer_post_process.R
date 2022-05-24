a76e2ee14b423c399008e0f2e75c53919e06a2af
aleighbrown <brown.annaleigh@gmail.com>
2022-05-24 15:58:51 +0100

    more reasonable memory

--- scripts/modulizer_post_process.R
+++ scripts/modulizer_post_process.R
@@ -22,7 +22,6 @@ mean_exon_psi <- function(my_df, columnA,columnB){
 }
 
 top_folder = "/Users/annaleigh/cluster/alb_projects/data/tdp_ko_collection/invitro_majiq/majiq/"
-middle_folder = 'delta_psi_voila_tsv' 
 bottom_folder = 'controlBrownSKNDZ-TDP43KDBrownSKNDZ'
 
 baseline = str_split(bottom_folder,"-")[[1]][1]
@@ -32,28 +31,26 @@ contrastpsi = paste0(contrast, '_median_psi')
 
 
 # read in the annotated junctions -----------------------------------------
+middle_folder = 'delta_psi_voila_tsv' 
 my_junctions = fread(file.path(top_folder,middle_folder,paste0(bottom_folder,'_annotated_junctions.csv')))
-
+middle_folder = 'modulizers/' 
+junctions_mod = fread(file.path(top_folder,middle_folder,paste0(bottom_folder,'/junctions.tsv')))
+junctions_mod[,paste_into_igv_junction := paste0(seqid,":",junction_coord)]
+junctions_mod[paste_into_igv_junction %in% all_cryptic_splicing$paste_into_igv_junction]
 all_cryptic_splicing = my_junctions %>% 
     filter(my_junctions[,12] < 0.05 & my_junctions[,13] > 0.1) %>% 
-    select(1,2,3,5,6,7,12,13,21,35)
+    select(1,2,3,5,6,7,12,13,21,35) %>% 
+    left_join(junctions_mod[,.(paste_into_igv_junction,module_event_combination)]) %>% 
+    unique() %>% 
+    mutate(module_event_combination = gsub("\\^\\d+","",module_event_combination)) %>% 
+    separate_rows(module_event_combination,sep = "\\|") %>% 
+    unique() 
 
-cryptic_exon_junctions = all_cryptic_splicing %>% 
-    filter(junc_cat %in% c("novel_donor","novel_acceptor","none"))
-
-skiptic_junctions = all_cryptic_splicing %>% 
-    filter(junc_cat %in% c("novel_exon_skip"))
-
-novel_combos = all_cryptic_splicing %>% 
-    filter(junc_cat %in% c("novel_combo"))
 
 # Read in the summary to find out what kind of files I'll need for each type -----------------------------------------
 middle_folder = 'modulizers/' 
-my_junctions = fread(file.path(top_folder,middle_folder,paste0(bottom_folder,'summary.tsv')))
 
 # cassette exons -----------------------------------------------------------
-
-
 folder_path  = file.path(top_folder,middle_folder, bottom_folder)
 
 cassette = fread(file.path(folder_path,"cassette.tsv"))
